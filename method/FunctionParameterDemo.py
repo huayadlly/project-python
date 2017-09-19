dd50044815b6f1435e97d378c1473b090943d173
huayadlly <huayadlly@gmail.com>
2017-09-20 00:40:52 +0800

    函数的参数；

--- method/FunctionParameterDemo.py
+++ method/FunctionParameterDemo.py
@@ -29,5 +29,57 @@ def enroll(name, gander, age=6, city='BeiJing'):
 
 
 enroll('Tom', 'boy')
-enroll('Jerry', 'boy', 9)
+enroll('Jerry', 'boy', 9)  # 顺序的方式给默认参数赋值，city没有赋值任然使用默认值
 enroll('Lucy', 'girl', 8, 'London')
+enroll('hyd', 'boy', city='Henan')  # 不按照顺序给默认参数赋值，需要指定参数的名字
+
+
+# 默认参数必须指向不变对象！错误demo演示如下：
+def wrong_demo(L=[]):
+    L.append('END')
+    return L
+
+
+print(wrong_demo([1, 2, 3]))
+print(wrong_demo())
+print(wrong_demo())  # 开始出现错误：“ ['END', 'END'] ”
+
+
+# 解决方法
+def fix_demo(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+
+print('---')
+print(fix_demo())
+print(fix_demo())
+
+"""
+    可变参数
+1.可变参数就是传入的参数个数是可变的，可以是1个、2个到任意个，还可以是0个
+"""
+
+
+def calc(number):
+    c_sum = 0
+    for item in number:
+        c_sum = c_sum + item * item
+    return c_sum
+
+
+print('平方和：', calc([1, 2, 3]))  # 传入的参数是一个集合或者tuple
+
+
+def calc(*numbers):
+    s = []
+    for i in numbers:
+        s.append(i)
+    print(s)
+    # v = calc(s)
+    return s
+
+
+print('又是平方和？：', calc(1, 2, 3))
